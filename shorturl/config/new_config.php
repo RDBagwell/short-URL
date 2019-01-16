1560e924315abd3c9fbb4c5b6fa83cb721dbd824
Robert Bagwell <erbagwell@yahoo.com>
2019-01-16 07:17:25 -0700

    Add files via upload

--- shorturl/config/new_config.php
+++ shorturl/config/new_config.php
@@ -4,3 +4,4 @@
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'urlshortener');
+    define('BASEROOT', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
